86ff881abffade964d43841c3120e860e2b4c61f
Igor Rinkovec <shiqqqi@gmail.com>
2018-04-12 20:31:36 +0200

    Bug fixes.

--- public/js/score-loader.js
+++ public/js/score-loader.js
@@ -18,7 +18,7 @@ Handlebars.registerHelper('getScore', function(collegeId, scores, options) {
 Handlebars.registerHelper('dateFormat', function(context, block) {
     if (window.moment) {
         var f = block.hash.format || "MMM Do, YYYY";
-        return moment(Date(context)).format(f);
+        return moment(context, moment.HTML5_FMT.DATETIME_LOCAL_SECONDS).format(f);
     } else {
         return context;   //  moment plugin not available. return data as is.
     }
